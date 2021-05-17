3b55789ca297c386e51b66c181c7e1a2f684651e
tianlinyang <tianlinyang303@gmail.com>
2021-05-17 15:48:50 -0400

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'RTX 3060ti',
+            catagory:'GPU',
+            image:'/images/p1.jpg',
+            price:1298,
+            brand:'Nvidia',
+            rating:4.5,
+            numReviews: 1023,
+            description: 'High performance graphic card',
+        },
+        {
+            _id:'2',
+            name:'RTX 3060',
+            catagory:'GPU',
+            image:'/images/p2.jpg',
+            price:1098,
+            brand:'Nvidia',
+            rating:2.5,
+            numReviews: 105,
+            description: 'High performance graphic card',
+        },
+
+        {
+            _id:'3',
+            name:'RTX 3070',
+            catagory:'GPU',
+            image:'/images/p3.jpg',
+            price:1498,
+            brand:'Nvidia',
+            rating:5,
+            numReviews: 2320,
+            description: 'High performance graphic card',
+        },
+        {
+            _id:'4',
+            name:'RTX 3080',
+            catagory:'GPU',
+            image:'/images/p4.jpg',
+            price:1899,
+            brand:'Nvidia',
+            rating:4.5,
+            numReviews: 101,
+            description: 'High performance graphic card',
+        },
+        {
+            _id:'5',
+            name:'RTX 3090',
+            catagory:'GPU',
+            image:'/images/p5.jpg',
+            price:2498,
+            brand:'Nvidia',
+            rating:4.0,
+            numReviews: 69,
+            description: 'High performance graphic card',
+        },
+        {
+            _id:'6',
+            name:'GTX 1080ti',
+            catagory:'GPU',
+            image:'/images/p6.jpg',
+            price:998,
+            brand:'Nvidia',
+            rating:5.0,
+            numReviews: 6989,
+            description: 'High performance graphic card',
+        }
+    ]
+}
+
+export default data;
